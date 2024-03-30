a49abc95545d364560222bc00bbb573a6ed06713
kawaneao1996 <matsuoyoshihiro0426@gmail.com>
2024-03-30 11:32:01 +0900

    formatの変更

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 import dayjs from 'dayjs';
-import {useTime} from  './useTime';
+import { useTime } from './useTime';
 
 export default function App() {
   const time = useTime(1000);
-  const formattedTime = dayjs(time).format('HH:mm:ss');
+  const formattedTime = dayjs(time).format('M月D日\nH時m分\ns秒');
   return (
     <View style={styles.container}>
-      <Text>
+      <Text style={styles.styledText}>
         {formattedTime}
       </Text>
     </View>
@@ -20,5 +20,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  styledText: {
+    fontSize: 63,
+    fontWeight: 'bold',
+    textAlign: 'right',
   },
 });
